7aef3a54f80ac0500c71f9a2ca799ddb68b4a22e
dgmodesto <douglasgomesmodesto@gmail.com>
2019-05-14 20:56:49 -0300

    atualizações novo video sofia

--- src/Componentes/contato.jsx
+++ src/Componentes/contato.jsx
@@ -1,11 +1,59 @@
 import React, { Component } from 'react';
 
+import api from '../servicos/api'
+
 class Contato extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            assunto: "",
+            email: "",
+            mensagem: ""
+        };
+
+        this.onChange = (evento) => {
+
+            //isso é feito pára não alterar o objeto state diretamento e com isso evitar mutações.
+            const state = Object.assign({}, this.state);
+            const campo = evento.target.name;
+            console.log(state);
+
+            state[campo] = evento.target.value;
+            this.setState(state);
+
+        };
+
+        this.onSubmit = async (evento) => {
+            evento.preventDefault();
+
+            if (this.state.assunto === '' || this.state.email === '' || this.state.mensagem === '') {
+                alert("Por favor, preencha todos os campos");
+            }
+
+            var objPostJson = {
+                Destino: this.state.email,
+                Assunto: this.state.assunto,
+                Mensagem: this.state.mensagem
+            }
+
+            //console.log(JSON.stringify(objPostJson));
+
+            const response = await api.post('api/email/', objPostJson)
+                .then(resp => {
+                    alert(resp.data);
+                 })
+                .catch(error => {
+                    console.log(error)
+            });
+
+        };
+    }
+
     render() {
         return (
             <div>
-                {/*Container (Contact Section) */} 
-      <div id='contact' className='container-fluid bg-grey'>
+                {/*Container (Contact Section) */}
+                <div id='contact' className='container-fluid bg-grey'>
                     <h2 className='text-center'>CONTATO</h2>
                     <div className='row'>
                         <div className='col-sm-5'>
@@ -17,16 +65,16 @@ class Contato extends Component {
                         <div className='col-sm-7 '>
                             <div className='row'>
                                 <div className='col-sm-6 form-group'>
-                                    <input className='form-control' id='name' name='name' placeholder='Nome' type='text' required />
+                                    <input className='form-control' id='assunto' name='assunto' value={this.state.assunto} onChange={this.onChange} placeholder='assunto' type='text' required />
                                 </div>
                                 <div className='col-sm-6 form-group'>
-                                    <input className='form-control' id='email' name='email' placeholder='Email' type='email' required />
+                                    <input className='form-control' id='email' name='email' value={this.state.email} onChange={this.onChange} placeholder='Email' type='email' required />
                                 </div>
                             </div>
-                            <textarea className='form-control' id='comments' name='comments' placeholder='Commentário' rows='5'></textarea><br />
+                            <textarea className='form-control' id='mensagem' name='mensagem' value={this.state.mensagem} onChange={this.onChange} placeholder='digite aqui ...' rows='5'></textarea><br />
                             <div className='row'>
                                 <div className='col-sm-12 form-group'>
-                                    <button className='btn btn-default pull-right' type='submit'>Enviar</button>
+                                    <button className='btn btn-primary pull-right' name='enviar' onClick={this.onSubmit} type='submit'>Enviar</button>
                                 </div>
                             </div>
                         </div>
---
+++ src/servicos/api.jsx
@@ -0,0 +1,7 @@
+import axios from 'axios';
+
+const api = axios.create({
+    baseURL: 'https://sofia-api.herokuapp.com/'
+});
+
+export default api;
